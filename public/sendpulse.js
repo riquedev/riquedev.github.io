297f468babbd85cd9ec25edf3d2bdab9ba0116b7
riquedev <rique_dev@hotmail.com>
2023-08-31 12:18:36 -0300

    :sparkles: added oSpP

--- public/sendpulse.js
+++ public/sendpulse.js
@@ -3,3 +3,7 @@ script.src = 'https://pop-ups.sendpulse.com/assets/loader.js';
 script.async = true;
 script.setAttribute("data-chats-widget-id", 'd275cdd3-58fb-413f-90f1-1fd4760bc439');
 document.head.appendChild(script);
+
+window.oSpP = {
+    id: "teste_sense"
+}
