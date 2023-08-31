8d6a0de5200805e7eb3be5844b7c86a0bf0bac49
riquedev <rique_dev@hotmail.com>
2023-08-31 12:13:46 -0300

    :sparkles: added sendpulse

---
+++ public/sendpulse.js
@@ -0,0 +1,5 @@
+script = document.createElement('script')
+script.src = 'https://pop-ups.sendpulse.com/assets/loader.js';
+script.async = true;
+script.setAttribute("data-chats-widget-id", 'd275cdd3-58fb-413f-90f1-1fd4760bc439');
+document.head.appendChild(script);
